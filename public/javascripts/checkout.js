f6bc0ee7918847e18fee8b6674ddd4d2b68224ba
paulcc <paulcc.two@gmail.com>
2009-08-20 17:46:52 +0100

    latest version of spree infrastructure files

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -10,7 +10,7 @@ $(function() {
   // hook up the continue buttons for each section
   for(var i=0; i < regions.length; i++) {     
     var section = regions[i];                          
-    $('#continue_' + section).click(function() { eval( "continue_button(this);") });   
+    $('#continue_' + section).click(function() { eval( "continue_button(this);"); return false; });
     
     // enter key should be same as continue button (don't submit form though)
     $('#' + section + ' input').bind("keyup", section, function(e) {
@@ -45,8 +45,8 @@ jQuery.fn.sameAddress = function() {
       return;
     }
     $('input#hidden_sstate').val($('input#hidden_bstate').val());
-    $("#billing input, #billing select").each(function() {
-      $("#shipping #"+ $(this).attr('id').replace('bill', 'ship')).val($(this).val());
+    $("#billing input, #billing select").each(function() {   
+      $("#shipping #"+ $(this).attr('id').replace('bill', 'shipment_attributes')).val($(this).val());
     })
     update_state('s');
   })
@@ -55,20 +55,19 @@ jQuery.fn.sameAddress = function() {
 //Initial state mapper on page load
 var state_mapper;
 var get_states = function() {
-  $.getJSON('/states.js', function(json) {
-    state_mapper = json;
-    $('span#bcountry select').val($('input#hidden_bcountry').val());
-    update_state('b');
-    $('span#bstate :only-child').val($('input#hidden_bstate').val());
-    $('span#scountry select').val($('input#hidden_scountry').val());
-    update_state('s');
-    $('span#sstate :only-child').val($('input#hidden_sstate').val());
-  });
+  $('span#bcountry select').val($('input#hidden_bcountry').val());
+  update_state('b');
+  $('span#bstate :only-child').val($('input#hidden_bstate').val());
+  $('span#scountry select').val($('input#hidden_scountry').val());
+  update_state('s');
+  $('span#sstate :only-child').val($('input#hidden_sstate').val());
 };
 
 // replace the :only child of the parent with the given html, and transfer
 //   {name,id} attributes over, returning the new child
 var chg_state_input_element = function (parent, html) {
+  var errorlabel = parent.find('label');
+  errorlabel.remove();
   var child = parent.find(':only-child');
   var name = child.attr('name');
   var id = child.attr('id');
@@ -91,6 +90,9 @@ var chg_state_input_element = function (parent, html) {
 // TODO: better as sibling dummy state ?
 // Update the input method for address.state 
 var update_state = function(region) {
+  if ($('span#' + region + 'state').length == 0) { 
+    return;
+  } 
   var country        = $('span#' + region + 'country :only-child').val();
   var states         = state_mapper[country];
   var hidden_element = $('input#hidden_' + region + 'state');
@@ -179,11 +181,11 @@ var shift_to_region = function(active) {
     }
   }                                                                         
   if (active == 'confirmation') {
+    // indicates order is ready to be processed (as opposed to simply updated)
     $("input#final_answer").attr("value", "yes");    
     $('#continue_confirmation').removeAttr('disabled', 'disabled'); 
     $('#post-final').removeAttr('disabled', 'disabled'); 
   } else {
-    // indicates order is ready to be processed (as opposed to simply updated)
     $("input#final_answer").attr("value", "");
     // disable form submit
     $('div#checkout :submit').attr('disabled', 'disabled');
@@ -286,7 +288,7 @@ var update_shipping_methods = function(methods) {
     var i = $(document.createElement('input'))
                 .attr('id', this.id)
                 .attr('type', 'radio')
-                .attr('name', 'checkout[shipping_method_id]')
+                .attr('name', 'checkout[shipment_attributes][shipping_method_id]')
                 .val(this.id)
                 .click(function() { $('div#methods input').attr('checked', ''); $(this).attr('checked', 'checked'); });
     if($(methods).length == 1) {
@@ -366,6 +368,7 @@ var ajax_login = function() {
       if (result) {
         $('div#already_logged_in').show();
         $('div#register_or_guest').hide();
+        update_addresses(result);
         update_login();
       } else {
         registration_error("Invalid username or password.");
@@ -429,7 +432,7 @@ var update_login = function() {
     },      
     dataType: "html",
     success: function(result) {
-	 		$("div#login-bar").html(result);  
+      $("div#login-bar").html(result);
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
       // TODO (maybe do nothing)
